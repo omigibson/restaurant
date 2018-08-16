88d19147933999b742e93e38ab241b93831dcb61
Herman Byström <herman.bystrom@live.com>
2018-08-16 13:01:10 +0200

    possible to select a date

--- src/components/BookingCalendar.js
+++ src/components/BookingCalendar.js
@@ -6,7 +6,9 @@ class Booking extends React.Component {
   is the same data, but converted to Date-format. */
   state = {
     allBookings: null,
-    convertedBookings: []
+    convertedBookings: [],
+    stepCompleted: false,
+    dateSelected: null
   }
 
   /* Before the component is mounted fetchBookings is called and the result is
@@ -18,8 +20,26 @@ class Booking extends React.Component {
           /* After all the bookings are present in this.state.allBookings they
           are converted to the Date format through the convertBookingtoDates-method. */
           this.convertBookingstoDates();
+          this.initiateCalendarEventListeners();
         });
       })
+  }
+
+  /* Since the npm-package react-booking-calendar doesn't support click-events
+  this is a solution that is not very Reactesque, but solves the problem of
+  gathering the neccessary data through good old JavaScript. */
+  initiateCalendarEventListeners = () => {
+    const monthAndYear = document.getElementsByClassName("month-label")[0].innerText;
+    const dayBox = document.getElementsByClassName("day");
+    const arrayFromHTMLCollection = Array.from(dayBox);
+    arrayFromHTMLCollection.map((item) => {
+      item.addEventListener('click', () => {
+        const todayDate = item.childNodes[0].innerText;
+        const todaysFullDate = new Date(todayDate + ' ' + monthAndYear);
+        this.setState({ stepCompleted: true, dateSelected: todaysFullDate });
+      });
+    });
+
   }
 
   fetchBookings = () => {
@@ -32,7 +52,7 @@ class Booking extends React.Component {
   convertBookingstoDates = (props) => {
     if (this.state.allBookings) {
       let allConvertedBookings = [];
-      this.state.allBookings.map((booking, i) => {
+      this.state.allBookings.map((booking) => {
         allConvertedBookings.push(new Date(booking.date));
       });
       this.setState({ convertedBookings: allConvertedBookings });
@@ -41,16 +61,25 @@ class Booking extends React.Component {
 
   render = () => {
     /* Only render if this.state.convertedBookings returns true. */
-    if (this.state.convertedBookings) {
-      return (
-        <div className="booking-calendar-container">
-          <BookingCalendar bookings={this.state.convertedBookings} clickable={true} />
-        </div>
-      );
-    }
+    if (!this.state.stepCompleted) {
+      if (this.state.convertedBookings) {
+        return (
+          <div className="booking-calendar-container">
+            <BookingCalendar
+            disableHistory={true}
+            bookings={this.state.convertedBookings}
+            clickable={true}
+            />
+          </div>
+        );
+      }
     else {
       return null;
     }
+  }
+  else {
+    return <p> { this.state.dateSelected.toString() } </p>
+  }
   }
 }
 
