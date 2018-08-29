16d5ac38acf0b5f9784a3523dda26c79d7f794fb
Herman Byström <herman.bystrom@live.com>
2018-08-29 10:53:49 +0200

    animation

--- src/components/GuestComponent.js
+++ src/components/GuestComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Booking from './Booking';
+import { Transition } from 'react-spring';
 
 class GuestComponent extends React.Component {
 
@@ -33,20 +34,22 @@ class GuestComponent extends React.Component {
   render = () => {
       if (!this.state.stepCompleted) {
         return (
-          <div className='container'>
-            <h2>Select an amount of guests</h2>
-            <div className='amount-of-guests-container'>
-              { this.amountOfGuestsButtons({
-                  1: '1 Guests',
-                  2: '2 Guests',
-                  3: '3 Guests',
-                  4: '4 Guests',
-                  5: '5 Guests',
-                  6: '6 Guests'
-                })
-              }
-            </div>
-          </div >
+          <Transition from={{position: 'absolute', left: -400 }} enter={{ left: 400 }} leave={{ left: 1000 }}>
+            { styles => <div className='container' style={styles}>
+              <h2>Select an amount of guests</h2>
+              <div className='amount-of-guests-container'>
+                { this.amountOfGuestsButtons({
+                    1: '1 Guests',
+                    2: '2 Guests',
+                    3: '3 Guests',
+                    4: '4 Guests',
+                    5: '5 Guests',
+                    6: '6 Guests'
+                  })
+                }
+              </div>
+            </div> }
+          </Transition>
         );
       }
     else {
