658cefd37ce1c8795df74d919cfd8d5f5de3bc0f
Lisa Sundberg <hello@lisasundberg.com>
2018-08-30 10:29:45 +0200

    Put footer in separate component

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <footer>
+      <div className="footer__top flex">
+        <div className="address">
+          <h3>Find us</h3>
+          <address>
+            <p>Street 18</p>
+            <p>121 12 City of Dreams</p>
+            <p>COUNTRY</p>
+          </address>
+        </div>
+        <div className="our-story">
+          <h3>Our story</h3>
+          <p>
+              Founded in 2002 by Anna Svensson and Gittan Committa,
+              Nano Food is the manifestation of a Vietnamese-Swedish
+              love story. Seasonal, locally produced produce is our passion.
+          </p>
+          <p>Read the full story.</p>
+        </div>
+      </div>
+      <hr />
+      <div className="footer__bottom flex space-between">
+        <div><small>Terms & Conditions / Privacy Policy © Nano Food AB</small></div>
+        <div>
+          <a href="tel:0812345678">08 — 123 45 678</a>
+          <a href="email:info@nanofood.io">info@nanofood.io</a>
+          <a href="http://facebook.com/nanofood">Facebook</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
