9afccf8c20a43d40d613ad654a8b3df619ee3aaf
Lisa Sundberg <hello@lisasundberg.com>
2018-08-30 14:32:57 +0200

    Add link to Menu-page in top menu

--- src/components/App.js
+++ src/components/App.js
@@ -50,8 +50,9 @@ class App extends Component {
             <header className="navbar-header flex hcenter">
               <ul>
                 <li><Link to="/home">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
                 <li><Link to="/booking">Booking</Link></li>
+                <li><Link to="/about">About</Link></li>
               </ul>
             </header>
 
