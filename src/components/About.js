5cef8529a03d7e346500d3124bab22f74708092f
omigibson <oumie.gibba@gmail.com>
2018-09-04 15:20:02 +0200

    Fixed typo

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ const About = () => {
                   <h2>Vietnamese cuisine meets Swedish tradition</h2>
                   <p>Founded in 2002 by best friends Anna Svensson and Gittan Committa, Nano Food is the manifestation of a Vietnamese-Swedish love story.
                   The duo found their passion for seasonal, locally produced food when traveling Vietnam. They then got into prestiguos Hoi An Chef School where they learned how to perfect the art of traditional vietnamese cooking.
-                  Back in Sweden, they created their own scandinavian style of the Vietnamese kitchen. Nano Food is simply yhe best of two worlds!
+                  Back in Sweden, they created their own scandinavian style of the Vietnamese kitchen. Nano Food is simply the best of two worlds!
                   </p>
                 </div>
               </div>
