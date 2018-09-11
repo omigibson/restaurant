5919ad653412283485b54e6f39659ef830fb54fb
omigibson <oumie.gibba@gmail.com>
2018-09-11 17:02:58 +0200

    Imported Provider and createStore. Created rootREducer. WIP.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById("root"));
-registerServiceWorker();
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    , document.getElementById("root"));
+  registerServiceWorker();
+  </Provider>
