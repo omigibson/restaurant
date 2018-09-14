7f31084857f9d604b93b0bb9f9a2021f04655d35
Patrick Edqvist <patrick.edqvist@gmail.com>
2018-09-14 10:21:55 +0200

    Corrected merge in bookings reducer

--- src/reducers/bookings.js
+++ src/reducers/bookings.js
@@ -7,7 +7,7 @@ const initialState = {}
 const bookings = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_BOOKINGS_SUCCESS:
-      return Object.assign({}, state, {bookings: action.payload.bookings});
+      return Object.assign({}, state, action.payload.bookings);
     default:
       return state;
   }
