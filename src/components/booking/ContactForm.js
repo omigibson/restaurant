666c3de631b2eb32923c4ad6e5914c023d7d4367
omigibson <oumie.gibba@gmail.com>
2018-10-02 10:42:14 +0200

    Trying to redirect to confirmation after email is sent

--- src/components/booking/ContactForm.js
+++ src/components/booking/ContactForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from 'react-router-dom';
 import ProgressBar from "./BookingProgress";
 import Confirmation from "./Confirmation";
 import { Transition } from "react-spring";
@@ -82,21 +83,10 @@ class ContactForm extends React.Component {
       time: this.props.viewstate.get('selectedTime'),
       userID: '',
       hash: hash
-      }
+    }
     )
 
-        // .then((bookingDetailsResponse) => {
-        //   /* Sends JSON to send_email.php – a file that sends a confirmation E-Email
-        //   to the user. */
-        //   this.props.sendToAPI({
-        //     userName: this.state.userName,
-        //     userEmail: this.state.userEmail,
-        //     userTelephone: this.state.userTelephone,
-        //     guests: this.props.bookingDetails.amountOfGuests,
-        //     date: this.props.convertDateObjectToString(this.props.bookingDetails.dateSelected),
-        //     time: this.props.bookingDetails.timeSelected,
-        //     hash: hash
-        //   }, "send_email.php")
+
         //     .then((emailResponse) => {
         //       this.setState({ allBookingDetails: emailResponse, stepCompleted: true }, () => {
         //         /* We set the progress bar to full width here so we don't need
@@ -105,90 +95,13 @@ class ContactForm extends React.Component {
         //       });
         //     })
         // });
+
     }
 
   render = () => {
-    if (!this.state.stepCompleted) {
-    return (
-      <React.Fragment>
-        <Transition
-          from={{right: "-50%", position: "absolute", transform: "translateX(100%)" }}
-          enter={{right: "50%", transform: "translateX(50%)" }}
-          leave={{ transform: "translateX(-200%)" }}
-        >
-        { styles =>
-          <div className="container booking-step" style={styles}>
-              <div className="contact-form">
-                <div className="booking-details">
-                  <ul>
-                    <li><span className="bold">Guests:</span> { this.props.viewstate.get('amountOfGuests') } </li>
-                    <li><span className="bold">Date:</span> { this.props.viewstate.get('selectedDate') } </li>
-                    <li><span className="bold">Time:</span> { this.props.viewstate.get('selectedTime') + ":00" } </li>
-                  </ul>
-                </div>
-                <form>
-                  <h2>3/4 Give us your contact details</h2>
-                  <label htmlFor="userName">Full name</label>
-                  <span className={ this.state.nameErrorMessage }> (This input field is not filled out correctly)</span>
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    name="userName"
-                    onChange={this.handleChange.bind(this)}
-                    className={ this.state.usernameStyle }
-                  />
-                <label htmlFor="userEmail">E-mail</label>
-                  <span className={ this.state.emailErrorMessage }> (This input field is not filled out correctly)</span>
-                  <input
-                    type="email"
-                    placeholder="E-mail"
-                    name="userEmail"
-                    onChange={this.handleChange.bind(this)}
-                    className={ this.state.emailStyle }
-                  />
-                  <label htmlFor="userTelephone">Phone number</label>
-                  <span className={ this.state.phoneErrorMessage }> (This input field is not filled out correctly)</span>
-                  <input
-                    type="tel"
-                    placeholder="Telephone"
-                    name="userTelephone"
-                    onChange={this.handleChange.bind(this)}
-                    className={ this.state.phoneStyle }
-                  />
-                <span className={ this.state.checkboxErrorMessage }>You need to check the box in order to make a booking</span>
-                  <div className="consent-container flex">
-                    <input
-                      type="checkbox"
-                      onChange={() => this.setState({ consent: !this.state.consent })}
-                    />
-                    <label htmlFor="consent">
-                      I give Nano Food consent to store and manage the information
-                      I left here, in order to enable table reservation.
-                    </label>
-                  </div>
-                  <div className="flex column">
-                    <span className="gdpr-info">
-                      Nano Food follows the EU regulation General Data Protection Regulation (GDPR).
-                      Read more <a href="https://www.datainspektionen.se/other-lang/in-english/the-general-data-protection-regulation-gdpr2/">here</a>.
-                      Your data will be removed when the date for your booking has passed.</span>
-                    <button
-                      className="contact-form-button button green"
-                      type="button"
-                      value="Book"
-                      onClick={ () => this.areAllInputsValid() ? this.sendAllToAPI() : this.giveFeedbackToUser() }
-                    >
-                    Send
-                    </button>
-                  </div>
-                </form>
-              </div>
-        </div> }
-        </Transition>
-        <ProgressBar progressValue="75"/>
-      </React.Fragment>
-      );
-    }
-    else {
+    switch (this.props.viewstate.bookingCompleted) {
+      case undefined:
+      console.log('Not completed')
       return (
         <React.Fragment>
           <Transition
@@ -197,23 +110,114 @@ class ContactForm extends React.Component {
             leave={{ transform: "translateX(-200%)" }}
           >
           { styles =>
-            <div className="container flex hcenter" style={styles}>
-              <Confirmation
-                //setAppState={ this.props.setAppState }
-                // name={ this.state.allBookingDetails.userName }
-                // date={ this.state.allBookingDetails.date }
-                // time={ this.state.allBookingDetails.time + ":00" }
-                // guests={ this.state.allBookingDetails.guests }
-              />
-            </div>
-          }
-        </Transition>
-        <ProgressBar progressValue="100"/>
-      </React.Fragment>
-      );
+            <div className="container booking-step" style={styles}>
+                <div className="contact-form">
+                  <div className="booking-details">
+                    <ul>
+                      <li><span className="bold">Guests:</span> { this.props.viewstate.get('amountOfGuests') } </li>
+                      <li><span className="bold">Date:</span> { this.props.viewstate.get('selectedDate') } </li>
+                      <li><span className="bold">Time:</span> { this.props.viewstate.get('selectedTime') + ":00" } </li>
+                    </ul>
+                  </div>
+                  <form>
+                    <h2>3/4 Give us your contact details</h2>
+                    <label htmlFor="userName">Full name</label>
+                    <span className={ this.state.nameErrorMessage }> (This input field is not filled out correctly)</span>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      name="userName"
+                      onChange={this.handleChange.bind(this)}
+                      className={ this.state.usernameStyle }
+                    />
+                  <label htmlFor="userEmail">E-mail</label>
+                    <span className={ this.state.emailErrorMessage }> (This input field is not filled out correctly)</span>
+                    <input
+                      type="email"
+                      placeholder="E-mail"
+                      name="userEmail"
+                      onChange={this.handleChange.bind(this)}
+                      className={ this.state.emailStyle }
+                    />
+                    <label htmlFor="userTelephone">Phone number</label>
+                    <span className={ this.state.phoneErrorMessage }> (This input field is not filled out correctly)</span>
+                    <input
+                      type="tel"
+                      placeholder="Telephone"
+                      name="userTelephone"
+                      onChange={this.handleChange.bind(this)}
+                      className={ this.state.phoneStyle }
+                    />
+                  <span className={ this.state.checkboxErrorMessage }>You need to check the box in order to make a booking</span>
+                    <div className="consent-container flex">
+                      <input
+                        type="checkbox"
+                        onChange={() => this.setState({ consent: !this.state.consent })}
+                      />
+                      <label htmlFor="consent">
+                        I give Nano Food consent to store and manage the information
+                        I left here, in order to enable table reservation.
+                      </label>
+                    </div>
+                    <div className="flex column">
+                      <span className="gdpr-info">
+                        Nano Food follows the EU regulation General Data Protection Regulation (GDPR).
+                        Read more <a href="https://www.datainspektionen.se/other-lang/in-english/the-general-data-protection-regulation-gdpr2/">here</a>.
+                        Your data will be removed when the date for your booking has passed.</span>
+                      <button
+                        className="contact-form-button button green"
+                        type="button"
+                        value="Book"
+                        onClick={ () => this.areAllInputsValid() ? this.sendAllToAPI() : this.giveFeedbackToUser() }
+                      >
+                      Send
+                      </button>
+                    </div>
+                  </form>
+                </div>
+          </div> }
+          </Transition>
+          <ProgressBar progressValue="75"/>
+        </React.Fragment>
+        );
+        case true:
+        console.log('completed')
+          return <Redirect to={'/booking/confirmation'} />
+        default:
+        console.log('default');
+          return null
+
+        }
+//     else {
+//       // return (
+// // if (this.props.viewstate.bookingCompleted === true) {
+//         return <Redirect to={'/booking/confirmation'} />
+//       }
+
+      //   <React.Fragment>
+      //     <Transition
+      //       from={{right: "-50%", position: "absolute", transform: "translateX(100%)" }}
+      //       enter={{right: "50%", transform: "translateX(50%)" }}
+      //       leave={{ transform: "translateX(-200%)" }}
+      //     >
+      //     { styles =>
+      //       <div className="container flex hcenter" style={styles}>
+      //         <Confirmation
+      //           //setAppState={ this.props.setAppState }
+      //           // name={ this.state.allBookingDetails.userName }
+      //           // date={ this.state.allBookingDetails.date }
+      //           // time={ this.state.allBookingDetails.time + ":00" }
+      //           // guests={ this.state.allBookingDetails.guests }
+      //         />
+      //       </div>
+      //     }
+      //   </Transition>
+      //   <ProgressBar progressValue="100"/>
+      // </React.Fragment>
+      // );
     }
   }
-}
+
 export default connect(ContactForm, { updateViewstate, makeBookingRequest}, (store) => ({
   viewstate: store.viewstate,
 }))
